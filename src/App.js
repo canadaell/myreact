714e554b0b2e78c2f8a06d3b9d190ebf25e75082
canadaell <mingdezhou1@outlookoutlook.com>
2024-03-11 23:15:24 +0000

    create project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+//项目的根组件
+//app->index.js->index.html(root)
+const index=100;
+function getname(){
+  return 'zhangsan';
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      this is my first react app
+      {/*引号传递字符串*/}
+      {'zifuchuan'}
+      {/*识别js变量 */}
+      {index}
+      {/*调用函数*/}
+      {getname()}
+      {/*方法调用*/}
+      {new Date().getDate()}
+      {/*使用js对象*/}
+      <div style={{color:'red',fontSize:'20px'}}>这是一个div</div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
+//项目的入口文件
+
+
+//react核心包
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+//项目的根组件
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//渲染到idweiroot的dom节点上
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
